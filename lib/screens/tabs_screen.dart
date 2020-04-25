acd0e21fb21ce29cd76900042084167316252b15
Lincoln Arrais <lincoln_arrais@hotmail.com>
2020-04-25 15:03:11 -0300

    Criamos e implementamos um componente Drawer para estender a navegação dentro na aplicação. Criamos, mas não implementamos, uma tela de configurações SettingsScreen.

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'categories_screen.dart';
 import 'favorite_screen.dart';
+import '../components/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -27,6 +28,7 @@ class _TabsScreenState extends State<TabsScreen> {
           _screens[_selectedScreenIndex]['title'],
         ),
       ),
+      drawer: MainDrawer(),
       body: _screens[_selectedScreenIndex]['screen'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectScreen,
