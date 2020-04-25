c21efbf1ee49fd48386d9cf0fbd2d8821b251cde
Lincoln Arrais <lincoln_arrais@hotmail.com>
2020-04-25 14:08:09 -0300

    Movemos a TabBar (barra de abas) do bottom da AppBar (na parte superior da tela) para a parte inferior usando a bottomNavigationBar do Scaffold.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'screens/tabs_screen.dart';
 import 'screens/categories_meals_screen.dart';
-import 'screens/categories_screen.dart';
 import 'screens/meal_detail_screen.dart';
 
 import 'utils/app_routes.dart';
