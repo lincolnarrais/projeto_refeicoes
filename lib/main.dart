071e515ee5081ed0d36ddd607fc2060ec68d35cf
Lincoln Arrais <lincoln_arrais@hotmail.com>
2020-04-25 13:44:49 -0300

    Na tela principal desenvolvemos uma navegação usando tabs (abas) na parte superior da tela. Criamos uma tela de abas TabsScreen (a partir de então a nova tela principal), juntamente com a aba de categorias que mostrará a já existente tela de categorias, que era a tela principal até então, e a aba de favoritos, que mostrará a tela de favoritos que também criamos.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'screens/tabs_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'screens/categories_screen.dart';
 import 'screens/meal_detail_screen.dart';
+
 import 'utils/app_routes.dart';
 
 void main() => runApp(MyApp());
@@ -25,7 +27,7 @@ class MyApp extends StatelessWidget {
             ),
       ),
       routes: {
-        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
       },
