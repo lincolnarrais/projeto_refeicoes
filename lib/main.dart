cfc332f9735cbc6fddac8ce3ad8b0fc5891c3a75
Lincoln Arrais <lincoln_arrais@hotmail.com>
2020-04-25 18:39:27 -0300

    (Versão final da aplicação no curso). Implementamos a parte das refeições favoritas tanto para adicionar aos favoritos quanto para remover dos favoritos. Porém um problema não foi resolvido e permaneceu ao remover uma refeição dos favoritos.

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   Settings settings = Settings();
   List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
 
   void _filterMeals(Settings settings) {
     setState(() {
@@ -31,9 +32,24 @@ class _MyAppState extends State<MyApp> {
         final hasLactoseFilter = settings.isLactoseFree && !meal.isLactoseFree;
         final isVeganFilter = settings.isVegan && !meal.isVegan;
         final isVegetarianFilter = settings.isVegetarian && !meal.isVegetarian;
-        return !hasGlutenFilter && !hasLactoseFilter && !isVeganFilter && !isVegetarianFilter;
+        return !hasGlutenFilter &&
+            !hasLactoseFilter &&
+            !isVeganFilter &&
+            !isVegetarianFilter;
       }).toList();
     });
+  }
+
+  void _toggleFavorite(Meal meal) {
+    setState(() {
+      _favoriteMeals.contains(meal)
+          ? _favoriteMeals.remove(meal)
+          : _favoriteMeals.add(meal);
+    });
+  }
+
+  bool _isFavorite(Meal meal) {
+    return _favoriteMeals.contains(meal);
   }
 
   @override
@@ -53,9 +69,10 @@ class _MyAppState extends State<MyApp> {
             ),
       ),
       routes: {
-        AppRoutes.HOME: (ctx) => TabsScreen(),
-        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(_availableMeals),
-        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+        AppRoutes.HOME: (ctx) => TabsScreen(_favoriteMeals),
+        AppRoutes.CATEGORIES_MEALS: (ctx) =>
+            CategoriesMealsScreen(_availableMeals),
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(_toggleFavorite, _isFavorite),
         AppRoutes.SETTINGS: (ctx) => SettingsScreen(settings, _filterMeals),
       },
     );
